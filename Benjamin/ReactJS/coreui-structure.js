6b8209662c84cb39f73448f18c5c643739f47fec
Hồng Hải <honghai.js6@gmail.com>
2018-11-07 18:02:35 +0700

    Update Benjamin/ReactJS/coreui-structure.js

---
+++ Benjamin/ReactJS/coreui-structure.js
@@ -0,0 +1,15 @@
+/*
+- Initial : index.js
+    - servicewebworker : lưu lại bản html chứa app
+    - load Main Object : APP.js
+    -> dùng ReactDOM gắn App object vào html => chạy App.js
+
+    App.js :
+        - react-router-dom lib
+        - load các css toàn bộ
+        - load các trang ~ big components cấp 1 :
+
+        -> Dùng HashRouter - Switch - Route của react-router-dom : để chứa các big component cấp 1
+        -> Chạy 1 big components có path : '/'
+    
+*/
