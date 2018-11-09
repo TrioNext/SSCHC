bae2c1fcddfa43e0903b10e297270a72e79149af
Hồng Hải <honghai.js6@gmail.com>
2018-11-09 17:54:56 +0700

    Update src/_nav.js

--- src/_nav.js
+++ src/_nav.js
@@ -1,7 +1,7 @@
 export default {
   items: [
     {
-      name: 'Công ty',
+      name: 'Tổng quan',
       url: '/dashboard',
       icon: 'icon-speedometer',
       badge: {
@@ -11,7 +11,7 @@ export default {
     },
     {
       title: true,
-      name: 'Theme',
+      name: 'CUIR Co.,ltd', // TÊN ĐƠN VỊ SỬ DỤNG
       wrapper: {            // optional wrapper object
         element: '',        // required valid HTML5 element tag
         attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
@@ -19,243 +19,162 @@ export default {
       class: ''             // optional class names space delimited list for title item ex: "text-center"
     },
     {
-      name: 'Colors',
-      url: '/theme/colors',
-      icon: 'icon-drop',
+      name: 'Công Ty',
+      url: '/company',
+      icon: 'icon-compass',
+      children: [
+        {
+          name: 'Nhân Viên',
+          url: '/company/user',
+          icon: '',
+        },
+        {
+          name: 'Cài đặt công ty',
+          url: '/setting/company',
+          icon: '',
+        },
+      ],
     },
     {
-      name: 'Typography',
-      url: '/theme/typography',
-      icon: 'icon-pencil',
+      name: 'Nhà Kho',
+      url: '/inventory',
+      icon: 'icon-drawer',
+      children:[
+        {
+          name: 'Nhập - Xuất Kho',
+          url: '/inventory/receipt',
+          icon: '',
+        },
+        {
+          name: 'Điều chuyển kho',
+          url: '/inventory/producttranf',
+          icon: '',
+        },
+        {
+          name: 'Mua Hàng',
+          url: '/inventory/po',
+          icon: '',
+        },
+        {
+          name: 'Cài đặt kho',
+          url: '/setting/inventory',
+          icon: '',
+        },
+      ]
     },
+    {
+      name: 'Sổ Tiền',
+      url: '/cashflow',
+      icon: 'icon-briefcase',
+      children:[
+        {
+          name:'Phiếu thu - Phiếu chi',
+          url:'/cashflow/view',
+          icon:''
+        },
+        {
+          name:'Tổng quan Thu - Chi',
+          url:'/cashflow/status',
+          icon:''
+        },
+        {
+          name:'Cài đặt Sổ Tiền',
+          url:'/setting/cashflow',
+          icon:''
+        },
+      ]
+    },
+    {
+      name: 'Khách Hàng',
+      url: '/customer',
+      icon: 'icon-people',
+      children:[
+        {
+          name:'Danh sách khách hàng',
+          url:'/customer/view',
+          icon:''
+        },
+        {
+          name:'Cài đặt khách hàng',
+          url:'/customer/setting',
+          icon:''
+        }
+
+      ]
+    },
+    {
+      name: 'Bán Hàng',
+      url: '/order',
+      icon: 'icon-screen-desktop',
+      children:[
+        {
+          name:'Danh sách đơn hàng',
+          url:'/order/view',
+          icon:''
+        },
+        {
+          name:'Báo cáo',
+          url:'/order/status',
+          icon:''
+        },
+        {
+          name:'Cài đặt bán hàng',
+          url:'/setting/order',
+          icon:''
+        }
+
+      ]
+    },
+    {
+      name: 'Chiến Dịch',
+      url: '/campaign',
+      icon: 'icon-magnet',
+      children:[
+        {
+          name:'Danh sách chiến dịch',
+          url:'/campaign/view',
+          icon:''
+        },
+        {
+          name:'Cài đặt chiến dịch',
+          url:'/seting/campaign',
+          icon:''
+        },
+      ]
+    },
+
     {
       title: true,
-      name: 'Components',
+      name: 'Webstore',
       wrapper: {
         element: '',
         attributes: {},
       },
     },
+
     {
-      name: 'Base',
-      url: '/base',
-      icon: 'icon-puzzle',
-      children: [
-        {
-          name: 'Breadcrumbs',
-          url: '/base/breadcrumbs',
-          icon: 'icon-puzzle',
-        },
-        {
-          name: 'Cards',
-          url: '/base/cards',
-          icon: 'icon-puzzle',
-        },
-        {
-          name: 'Carousels',
-          url: '/base/carousels',
-          icon: 'icon-puzzle',
-        },
-        {
-          name: 'Collapses',
-          url: '/base/collapses',
-          icon: 'icon-puzzle',
-        },
-        {
-          name: 'Dropdowns',
-          url: '/base/dropdowns',
-          icon: 'icon-puzzle',
-        },
-        {
-          name: 'Forms',
-          url: '/base/forms',
-          icon: 'icon-puzzle',
-        },
-        {
-          name: 'Jumbotrons',
-          url: '/base/jumbotrons',
-          icon: 'icon-puzzle',
-        },
-        {
-          name: 'List groups',
-          url: '/base/list-groups',
-          icon: 'icon-puzzle',
-        },
-        {
-          name: 'Navs',
-          url: '/base/navs',
-          icon: 'icon-puzzle',
-        },
-        {
-          name: 'Paginations',
-          url: '/base/paginations',
-          icon: 'icon-puzzle',
-        },
-        {
-          name: 'Popovers',
-          url: '/base/popovers',
-          icon: 'icon-puzzle',
-        },
-        {
-          name: 'Progress Bar',
-          url: '/base/progress-bar',
-          icon: 'icon-puzzle',
-        },
-        {
-          name: 'Switches',
-          url: '/base/switches',
-          icon: 'icon-puzzle',
-        },
-        {
-          name: 'Tables',
-          url: '/base/tables',
-          icon: 'icon-puzzle',
-        },
-        {
-          name: 'Tabs',
-          url: '/base/tabs',
-          icon: 'icon-puzzle',
-        },
-        {
-          name: 'Tooltips',
-          url: '/base/tooltips',
-          icon: 'icon-puzzle',
-        },
-      ],
-    },
-    {
-      name: 'Buttons',
-      url: '/buttons',
+      name: 'Cpanel',
+      url: '/cpanel',
       icon: 'icon-cursor',
-      children: [
-        {
-          name: 'Buttons',
-          url: '/buttons/buttons',
-          icon: 'icon-cursor',
-        },
-        {
-          name: 'Button dropdowns',
-          url: '/buttons/button-dropdowns',
-          icon: 'icon-cursor',
-        },
-        {
-          name: 'Button groups',
-          url: '/buttons/button-groups',
-          icon: 'icon-cursor',
-        },
-        {
-          name: 'Brand Buttons',
-          url: '/buttons/brand-buttons',
-          icon: 'icon-cursor',
-        },
-      ],
     },
-    {
-      name: 'Charts',
-      url: '/charts',
-      icon: 'icon-pie-chart',
-    },
-    {
-      name: 'Icons',
-      url: '/icons',
-      icon: 'icon-star',
-      children: [
-        {
-          name: 'CoreUI Icons',
-          url: '/icons/coreui-icons',
-          icon: 'icon-star',
-          badge: {
-            variant: 'info',
-            text: 'NEW',
-          },
-        },
-        {
-          name: 'Flags',
-          url: '/icons/flags',
-          icon: 'icon-star',
-        },
-        {
-          name: 'Font Awesome',
-          url: '/icons/font-awesome',
-          icon: 'icon-star',
-          badge: {
-            variant: 'secondary',
-            text: '4.7',
-          },
-        },
-        {
-          name: 'Simple Line Icons',
-          url: '/icons/simple-line-icons',
-          icon: 'icon-star',
-        },
-      ],
-    },
-    {
-      name: 'Notifications',
-      url: '/notifications',
-      icon: 'icon-bell',
-      children: [
-        {
-          name: 'Alerts',
-          url: '/notifications/alerts',
-          icon: 'icon-bell',
-        },
-        {
-          name: 'Badges',
-          url: '/notifications/badges',
-          icon: 'icon-bell',
-        },
-        {
-          name: 'Modals',
-          url: '/notifications/modals',
-          icon: 'icon-bell',
-        },
-      ],
-    },
-    {
-      name: 'Widgets',
-      url: '/widgets',
-      icon: 'icon-calculator',
-      badge: {
-        variant: 'info',
-        text: 'NEW',
-      },
-    },
+
     {
       divider: true,
     },
     {
       title: true,
-      name: 'Extras',
+      name: 'Cài đặt - Cấu hình',
     },
     {
-      name: 'Pages',
-      url: '/pages',
-      icon: 'icon-star',
-      children: [
-        {
-          name: 'Login',
-          url: '/login',
-          icon: 'icon-star',
-        },
-        {
-          name: 'Register',
-          url: '/register',
-          icon: 'icon-star',
-        },
-        {
-          name: 'Error 404',
-          url: '/404',
-          icon: 'icon-star',
-        },
-        {
-          name: 'Error 500',
-          url: '/500',
-          icon: 'icon-star',
-        },
-      ],
+      name: 'Thiết lập ứng dụng',
+      url: '/setting/general',
+      icon: 'icon-puzzle'
+    },
+    {
+      name: 'Thiết lập thiết bị',
+      url: '/setting/devices',
+      icon: 'icon-wrench'
     }
-
 
   ],
 };
