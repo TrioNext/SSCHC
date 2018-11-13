0712f18d4d986771952f8360b7eea9459f0834a1
Hồng Hải <honghai.js6@gmail.com>
2018-11-13 17:56:28 +0700

    Update src/config/server.js

--- src/config/server.js
+++ src/config/server.js
@@ -1,7 +1,42 @@
 
+import axios from 'axios';
+
 const server = {
   host:"http://localhost",
-  port:3333
+  port:3333,
+  base(){
+    return this.host+':'+this.port
+  },
+
+  setHeader(){
+
+    return  {
+      headers:{
+        "Content-Type": "application/json",
+        "Authorization": this.token(),
+        "cache-control": "no-cache"
+      }
+    }
+  },
+  token:function(){
+    return localStorage.getItem('feathers-jwt');
+  },
+
+  get(url,onSuccess,onError){
+      axios.get(
+              this.base()+url,
+              this.setHeader()
+            )
+            .then((response) => {
+                onSuccess(response.data)
+            },
+            (error) => {
+                var status = error.response.status
+                onError(error);
+              }
+            );
+  }
+
 }
 
 export default server;
