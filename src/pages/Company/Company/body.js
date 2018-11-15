f3335dd9fe19aeae2a47649ad248f06e6e4293a0
Hồng Hải <honghai.js6@gmail.com>
2018-11-15 17:57:26 +0700

    Update body.js

--- src/pages/Company/Company/body.js
+++ src/pages/Company/Company/body.js
@@ -1,6 +1,10 @@
 
 import React, { Component} from 'react';
 
+import Office from './Office';
+import Store from './Store';
+import User from './User';
+
 
 
 class CompanyBody extends Component{
@@ -10,9 +14,19 @@ class CompanyBody extends Component{
   }
 
   render(){
+
+    const onTab = this.props.onTab ;
+    const onAction = this.props.onAction;
+
+    
+
     return(
       <div className="detail" >
-        { this.props.children}
+
+        <Office tab={onTab} tabAction={ onAction } />
+        <Store tab={onTab} tabAction={onAction} />
+        <User tab={onTab} tabAction={onAction} />
+
       </div>
     )
   }
