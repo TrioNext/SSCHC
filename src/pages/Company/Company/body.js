4864e7fb4b1142b08fbaa24f077df5a8ca72c1c7
Hồng Hải <honghai.js6@gmail.com>
2018-11-16 17:55:07 +0700

    Update body.js

--- src/pages/Company/Company/body.js
+++ src/pages/Company/Company/body.js
@@ -10,7 +10,53 @@ import User from './User';
 class CompanyBody extends Component{
 
   constructor(props){
-    super(props)
+    super(props);
+
+    this.name = 'Company Main';
+    this.info = {}
+
+    this.data = {
+      offices:[],
+      stores:[],
+      users:[]
+    }
+
+    this.state = {
+      onTab:props.onTab,
+      onAction:'',
+      status:'',
+
+    }
+
+
+
+  }
+
+  onStateChange(newState){
+
+      /*keep private data*/
+      Object.assign(this.state,newState)
+
+      //alert(JSON.stringify(this.state))
+      /* share gia tri cho parent component dùng */
+      this.props.onStateChange(this.state);
+
+  }
+
+  onDataChange(type,newData){
+
+  }
+
+  /* NHẬN lệnh : từ NEW PROPS TỪ MAIN OBJECT*/
+  componentWillReceiveProps(newProps){
+
+    /* CẬP NHẬN STATE */
+    /* GUI CHO COMPONENT OFFICE : re-render*/
+    this.setState(Object.assign(this.state,newProps));
+
+
+
+
   }
 
   render(){
@@ -18,14 +64,12 @@ class CompanyBody extends Component{
     const onTab = this.props.onTab ;
     const onAction = this.props.onAction;
 
-    
-
     return(
       <div className="detail" >
 
-        <Office tab={onTab} tabAction={ onAction } />
-        <Store tab={onTab} tabAction={onAction} />
-        <User tab={onTab} tabAction={onAction} />
+        <Office onStateChange={ (newState)=>{ this.onStateChange(newState) } } onDataChange={ (newData)=>{ this.onDataChange('office',newData) } } onTab={onTab} tabAction={ onAction } />
+        <Store onStateChange={ (newState)=>{ this.onStateChange(newState) } } onDataChange={ (newData)=>{ this.onDataChange('store',newData) } }  onTab={onTab} tabAction={onAction} />
+        <User onStateChange={ (newState)=>{ this.onStateChange(newState) } } onDataChange={ (newData)=>{ this.onDataChange('user',newData) } } onTab={onTab} tabAction={onAction} />
 
       </div>
     )
