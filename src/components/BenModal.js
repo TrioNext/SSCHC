b3a31b3e8ad55aa6c500be578b6b9fc211d8f10a
Hồng Hải <honghai.js6@gmail.com>
2018-11-22 17:40:25 +0700

    Create BenModal.js

---
+++ src/components/BenModal.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Row, Col, Label,  Form, FormGroup,FormText, Input,
+  Popover, PopoverHeader, PopoverBody
+} from 'reactstrap';
+
+
+class BenModal extends Component{
+
+
+  constructor(props){
+
+    super(props)
+
+    this.state = {
+      onAction:this.props.onAction
+    }
+
+    this.modal = props.modal;
+    this.name = props.name;
+    this.refErr = props.refErr;
+
+
+  }
+  render(){
+    return(
+      <Modal  size="lg" isOpen={ this.modal.active } fade={false}   toggle={ ()=>{  this.modal.toggle() } } >
+        <ModalHeader toggle={ ()=>{ this.modal.toggle() } }> <i className="fa fa-plus"></i> { this.name }  </ModalHeader>
+
+        <ModalBody>
+          <Form>
+            { this.props.children }
+          </Form>
+        </ModalBody>
+
+        <div className="my-modal-footer">
+           <div className="float-right">
+               <div role="group" className="btn-group">
+                     <Button className="btn-ubuntu" onClick={ ()=>{ this.modal.toggle() } }> <i className="fa fa fa-reply"></i> Từ Chối  </Button>
+                     <Button className="btn-ubuntu-ok" onClick={ ()=>{ this.modal.onSubmit() } }> <i className="fa fa-chevron-circle-right"></i> Đồng Ý </Button>
+               </div>
+
+           </div>
+
+        </div>
+
+        <div className="modal-err " >
+           <div className="float-left form-err text-muted" ref={ this.refErr }>
+               status
+           </div>
+           <div className="float-right">
+             <a id="btnDel" hidden={ this.state.onAction === 'post' ? true : false  } className={'text-muted btn-delete ' } onClick={ ()=>{ this.modal.popover.toggle() } }>
+               <i className="fa fa-trash"></i> Xoá
+             </a>
+             <Popover placement="bottom" isOpen={this.modal.popover.active } target="btnDel"  toggle={ ()=>{ this.modal.popover.toggle() } }>
+               <PopoverHeader>Bạn có chắc chắn không?</PopoverHeader>
+               <PopoverBody className="text-center pa-15">
+                 <button onClick={ ()=>{  this.modal.popover.btnYes() } } className="btn btn-sm btn-success mr-20">Có</button>
+
+                 <button onClick={ ()=>{  this.modal.popover.toggle() } } className="btn btn-sm btn-secondary">Không</button>
+               </PopoverBody>
+             </Popover>
+           </div>
+        </div>
+
+
+      </Modal>
+    )
+  }
+}
+
+export default BenModal;
