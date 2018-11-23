baa490f5e433a091180f56d512db1f523493b06c
Hồng Hải <honghai.js6@gmail.com>
2018-11-23 17:52:51 +0700

    Create SelectDist.js

---
+++ src/components/SelectDist.js
@@ -0,0 +1,31 @@
+
+import React, { Component } from 'react';
+import {  Input } from 'reactstrap';
+
+/*
+SelectDist
+  props
+    modal : controller class
+        listDistrict:[]
+        onChangeDist()
+
+    selected: string
+*/
+function SelectDist(props){
+
+
+  const modal = props.modal ;
+
+  let list = [];
+  modal.listDistrict.map((item)=>{
+    list.push(<option id={item.id} key={item.id} value={ item.code } > { item.name_with_type } </option>)
+  })
+
+  return(
+    <Input onChange={ (e)=>{  modal.onChangeDist(e)  } }  type="select" defaultValue={ props.selected }>
+      {list}
+    </Input>
+  )
+}
+
+export default SelectDist ;
