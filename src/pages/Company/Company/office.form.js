9d600b6a876bfe961e7ee2b6a934c65e5ba43533
Hồng Hải <honghai.js6@gmail.com>
2018-11-23 17:53:17 +0700

    Update office.form.js

--- src/pages/Company/Company/office.form.js
+++ src/pages/Company/Company/office.form.js
@@ -1,12 +1,146 @@
 import React, {Component} from 'react';
 
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Row, Col, Label,  Form, FormGroup,FormText, Input,
-  Popover, PopoverHeader, PopoverBody
-} from 'reactstrap';
+import {  Row, Col, Label,  Form, FormGroup,FormText, Input } from 'reactstrap';
 
 import moment from 'moment';
 
 import BenModal from '../../../components/BenModal';
+import SelectCity from '../../../components/SelectCity';
+import SelectDist from '../../../components/SelectDist';
+import SelectHour from '../../../components/SelectHour';
+import SelectMinute from '../../../components/SelectMinute';
+
+
+function FrmR1(props){
+
+  const modal = props.modal;
+  return (
+    <Row form>
+      <Col md={4}>
+        <FormGroup>
+          <Label> Mã văn phòng <span className="text-danger">*</span></Label>
+          <Input type="text" onChange={ (e)=>{ modal.onChange('code', e);  } } defaultValue={ modal.form.code }  placeholder="Tạo mã" />
+        </FormGroup>
+      </Col>
+      <Col md={8}>
+        <FormGroup>
+          <Label> Tên văn phòng <span className="text-danger">*</span></Label>
+          <Input type="text" onChange={ (e)=>{ modal.onChange('name', e);  } } defaultValue={ modal.form.name }  placeholder="Nhập tên" />
+        </FormGroup>
+      </Col>
+    </Row>
+  )
+}
+
+
+
+function FrmR2(props){
+
+
+  const modal = props.modal ;
+  const form = props.modal.form;
+
+  return(
+    <Row form>
+      <Col md={4}>
+        <FormGroup>
+          <Label> Số ĐT <span className="text-danger">*</span></Label>
+          <Input type="text" onChange={ (e)=>{ modal.onChange('phone', e);  } } defaultValue={ modal.form.phone }  placeholder="nhập số ĐT" />
+        </FormGroup>
+      </Col>
+      <Col md={4}>
+        <FormGroup>
+          <Label> Tỉnh / Thành </Label>
+            <SelectCity modal={ modal } selected={ form.region_code} />
+        </FormGroup>
+      </Col>
+      <Col md={4}>
+        <FormGroup>
+          <Label> Quận/Huyện </Label>
+
+            <SelectDist modal={ modal } selected={ form.subregion_code } />
+        </FormGroup>
+      </Col>
+    </Row>
+  )
+}
+
+function FrmR3(props){
+
+  const modal = props.modal;
+  const { form } = props.modal ;
+  return (
+    <div>
+      <FormGroup>
+        <Label>Địa chỉ <span className="text-danger">*</span></Label>
+          <Input type="text" onChange={ (e)=>{ modal.onChange('address', e);  } } defaultValue = { form.address } placeholder="Nhập địa chỉ"/>
+      </FormGroup>
+
+      <FormGroup>
+        <Label>IP được chấm công</Label>
+          <Input type="text" onChange={ (e)=>{ modal.onChange('ip_chamcong', e);  } } defaultValue = { form.ip_chamcong }  placeholder="Nhập địa chỉ IP"/>
+      </FormGroup>
+    </div>
+  )
+}
+
+
+function FrmR4(props){
+
+  const modal = props.modal ;
+  const { form } =  props.modal;
+
+  const begin = moment('2018-11-20 '+form.working_begin).format('HH:mm').split(':');
+
+
+  return (
+    <Row form>
+      <Col md={6}>
+        <FormGroup>
+          <Label> Giờ làm việc </Label>
+
+            <SelectHour modal={ modal } selected={ Number(begin[0]) } />
+        </FormGroup>
+      </Col>
+      <Col md={6}>
+        <FormGroup>
+          <Label> . </Label>
+            <SelectMinute modal={ modal } selected={ Number(begin[1]) } />
+        </FormGroup>
+      </Col>
+    </Row>
+  )
+}
+
+function FrmR5(props){
+
+  const modal = props.modal ;
+  const { form } =  props.modal;
+
+
+  const end = moment('2018-11-20 '+form.working_end).format('HH:mm').split(':');
+
+
+  return (
+    <Row form>
+      <Col md={6}>
+        <FormGroup>
+          <Label> Giờ tan ca </Label>
+
+              <SelectHour modal={ modal } selected={  Number(end[0]) } />
+
+        </FormGroup>
+      </Col>
+      <Col md={6}>
+        <FormGroup>
+          <Label> . </Label>
+
+            <SelectMinute modal={ modal } selected={ Number(end[1]) } />
+        </FormGroup>
+      </Col>
+    </Row>
+  )
+}
 
 
 class OfficeForm extends Component{
@@ -20,71 +154,9 @@ class OfficeForm extends Component{
 
     this.modal = props.modal;
     this.name = props.name;
-    this.refErr = props.refErr;
-  }
+    
+    this.refErr = React.createRef();
 
-  SelectDist(props){
-
-    let list = [];
-    this.modal.listDistrict.map((item)=>{
-      list.push(<option id={item.id} key={item.id} value={ item.code } > { item.name_with_type } </option>)
-    })
-
-    return(
-      <Input onChange={ (e)=>{  this.modal.onChangeDist(e)  } }  type="select" defaultValue={ props.selected }>
-        {list}
-      </Input>
-    )
-  }
-
-  SelectCity(props){
-
-    let list = [] ;
-
-
-    this.modal.listCity.map((item)=>{
-      list.push(<option id={item.id} value={item.code} key={item.id} > { item.name } </option>)
-    })
-
-    return(
-      <Input onChange={ (e)=>{  this.modal.onChangeCity(e)  } }  type="select" defaultValue={ props.selected }>
-        {list}
-      </Input>
-    )
-  }
-
-  /* build item component*/
-  SelectHour(props){
-
-    let list = [] ;
-    for(let i=0 ; i < 24 ; i++){
-     const num = i < 10 ? '0'+i : i
-     list.push(<option key={i} value={ i }  > {  num +' giờ' } </option>)
-    }
-
-    return(
-      <Input onChange={(e)=>{ this.modal.onHourChange(props.type, e) }}  type="select" defaultValue={ props.selected }>
-        {list}
-      </Input>
-    )
-  }
-
-  SelectMinute(props){
-
-
-
-    let list = [] ;
-    for(let i=0 ; i < 60 ; i++){
-      const num = i < 10 ? '0'+i : i
-
-      list.push(<option key={i} value={ i } > {  num +' phút' } </option>)
-    }
-
-    return(
-      <Input onChange={(e)=>{  this.modal.onMinuteChange(props.type, e)  }}  type="select" defaultValue={ props.selected }>
-        {list}
-      </Input>
-    )
 
   }
 
@@ -92,100 +164,35 @@ class OfficeForm extends Component{
   componentWillReceiveProps(newProps){
 
       this.modal = newProps.modal;
-      this.state.onAction = newProps.onAction;
 
+      this.setState({
+        onAction:newProps.onAction
+      });
 
 
   }
+
+
+
 
   render(){
 
     const modalTitle = this.state.onAction ==='post' ? 'Tạo ': 'Cập nhật ';
 
-    const { form } =  this.modal;
-
-    const begin = moment('2018-11-20 '+form.working_begin).format('HH:mm').split(':');
-    const end = moment('2018-11-20 '+form.working_end).format('HH:mm').split(':');
-
-
     return (
 
-      <BenModal name={modalTitle} onAction={ this.state.onAction} modal={ this.modal } refErr={ this.refErr} >
-          <Row form>
-            <Col md={4}>
-              <FormGroup>
-                <Label> Mã văn phòng <span className="text-danger">*</span></Label>
-                <Input type="text" onChange={ (e)=>{ this.modal.onChange('code', e);  } } defaultValue={ this.modal.form.code }  placeholder="Tạo mã" />
-              </FormGroup>
-            </Col>
-            <Col md={8}>
-              <FormGroup>
-                <Label> Tên văn phòng <span className="text-danger">*</span></Label>
-                <Input type="text" onChange={ (e)=>{ this.modal.onChange('name', e);  } } defaultValue={ this.modal.form.name }  placeholder="Nhập tên" />
-              </FormGroup>
-            </Col>
-          </Row>
-          <Row form>
-            <Col md={4}>
-              <FormGroup>
-                <Label> Số ĐT <span className="text-danger">*</span></Label>
-                <Input type="text" onChange={ (e)=>{ this.modal.onChange('phone', e);  } } defaultValue={ this.modal.form.phone }  placeholder="nhập số ĐT" />
-              </FormGroup>
-            </Col>
-            <Col md={4}>
-              <FormGroup>
-                <Label> Tỉnh / Thành </Label>
-                  { this.SelectCity({selected:form.region_code})}
-              </FormGroup>
-            </Col>
-            <Col md={4}>
-              <FormGroup>
-                <Label> Quận/Huyện </Label>
-                  { this.SelectDist({parent:form.region_code, selected:form.subregion_code}) }
-              </FormGroup>
-            </Col>
-          </Row>
-          <FormGroup>
-            <Label>Địa chỉ <span className="text-danger">*</span></Label>
-              <Input type="text" onChange={ (e)=>{ this.modal.onChange('address', e);  } } defaultValue = { form.address } placeholder="Nhập địa chỉ"/>
-          </FormGroup>
+      <BenModal name={modalTitle + this.name } state={ this.state} modal={ this.modal } refErr={ this.refErr} >
 
-          <FormGroup>
-            <Label>IP được chấm công</Label>
-              <Input type="text" onChange={ (e)=>{ this.modal.onChange('ip_chamcong', e);  } } defaultValue = { form.ip_chamcong }  placeholder="Nhập địa chỉ IP"/>
-          </FormGroup>
+          <FrmR1 modal={ this.modal } />
 
-          <Row form>
-            <Col md={6}>
-              <FormGroup>
-                <Label> Giờ làm việc </Label>
-                  { this.SelectHour({selected:Number(begin[0]), type:'working_begin'}) }
-              </FormGroup>
-            </Col>
-            <Col md={6}>
-              <FormGroup>
-                <Label> . </Label>
-                  { this.SelectMinute({selected:Number(begin[1]), type:'working_begin'}) }
-              </FormGroup>
-            </Col>
-          </Row>
+          <FrmR2 modal={ this.modal } />
 
-          <Row form>
-            <Col md={6}>
-              <FormGroup>
-                <Label> Giờ tan ca </Label>
+          <FrmR3 modal={ this.modal } />
 
-                    { this.SelectHour({selected:Number(end[0]), type:'working_end'}) }
+          <FrmR4 modal={ this.modal } />
 
-              </FormGroup>
-            </Col>
-            <Col md={6}>
-              <FormGroup>
-                <Label> . </Label>
-                  { this.SelectMinute({selected:Number(end[1]), type:'working_end'}) }
-              </FormGroup>
-            </Col>
-          </Row>
+          <FrmR5 modal={ this.modal } />
+
       </BenModal>
 
 
