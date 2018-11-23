aa861f850573f36b8d86360296776fd734101976
Hồng Hải <honghai.js6@gmail.com>
2018-11-23 17:53:28 +0700

    Update User.js

--- src/pages/Company/Company/User.js
+++ src/pages/Company/Company/User.js
@@ -7,14 +7,14 @@ npm install --save ag-grid-enterprise
 
 import React, {Component} from 'react';
 
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Row, Col, Label } from 'reactstrap';
+import {  Row, Col } from 'reactstrap';
+
+import Model from '../../../config/model';
+
 
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
-
-
-
 
 
 
@@ -23,85 +23,110 @@ class User extends Component{
     constructor(props){
       super(props);
 
+      this.code = 'user';
+      this.name = 'Thành viên';
+
+      this.data = {
+        id:0,
+        list:[]
+      }
+
       this.state = {
-        tab:props.tab,
-        action:'',
+        onAction:'',
+        status:'',
 
-        name:'Nhân viên ',
-        modal:{
-          status:false
-        },
+        onTab:props.onTab
+      }
 
+      this.table = {
         columnDefs: [
 
 
                 {headerName: "SID",field: "id",width:120,checkboxSelection: true},
                 {headerName: "Họ & Tên", field: "name"},
-                {headerName: "Văn phòng", field: "office"},
-                {headerName: "Cấp bậc", field: "level"},
-                {headerName: "Loại hình công việc", field: "type"},
+                {headerName: "Văn phòng", field: "office_id"},
+                {headerName: "Cấp bậc", field: "job_level"},
+                {headerName: "Loại hình công việc", field: "job_type"},
                 {headerName: "Số Phone ", field: "phone"},
-                {headerName: "Người tạo ", field: "create_by"},
+                {headerName: "Người tạo ", field: "creator_id"},
                 {headerName: "Ngày ", field: "date_created"}
 
 
             ],
-        rowData: [
-                {
-                  id:1,
-                  name:'Benjamin',
-                  office:'Văn phòng quận 4',
-                  level:'Nhân viên',
-                  type:'Chuyên nghiệp',
-                  create_by:'mruan',
-                  date_created:'12-12-2012'
-                },
-                {
-                  id:2,
-                  name:'Takumi HD',
-                  office:'Văn phòng quận 11',
-                  level:'Nhân viên',
-                  type:'Chuyên nghiệp',
-                  create_by:'Benjamin',
-                  date_created:'12-12-2012'
-                }
-        ]
-
+            rowData: [
+                    {
+                      id:1,
+                      name:'Benjamin',
+                      office:'Văn phòng quận 4',
+                      level:'Nhân viên',
+                      type:'Chuyên nghiệp',
+                      create_by:'mruan',
+                      date_created:'12-12-2012'
+                    },
+                    {
+                      id:2,
+                      name:'Takumi HD',
+                      office:'Văn phòng quận 11',
+                      level:'Nhân viên',
+                      type:'Chuyên nghiệp',
+                      create_by:'Benjamin',
+                      date_created:'12-12-2012'
+                    }
+            ]
       }
 
-      this.toggleModal = this.toggleModal.bind(this)
+      this.setup();
     }
 
-    toggleModal(){
-      this.setState({
-        modal:{
-          status:!this.state.modal.status
-        },
-        action:''
+    setup(){
+      this.model = new Model('users');
 
-      });
-
-
-    }
-    onAction(newProps){
-
-
-
-      let active = newProps.tab ==='user' ? newProps.tabAction ==='create' ? true: false : false ;
-
-
-      this.setState({
-        tab:newProps.tab,
-        action:newProps.tabAction,
-        modal:{
-          status:active
-        }
-
+      this.model.set('paginate',{
+        p:0,
+        max:30,
+        sort_by:'id',
+        sort_type:'desc'
       })
 
 
+    }
+
+    setData(name,value){
+      this.data[name] = value ;
+    }
+
+    onStateChange(newState){
+
+      /* KEEP PRIVATE DATA : refesh inside compoents */
+      this.setState(Object.assign(this.state,newState));
+
+      /* trả giá tri về cho parent component sử dụng */
+      this.props.onStateChange(this.state);
+
 
     }
+
+    onDataChange(list){
+
+      /* TRẢ GIÁ TRỊ VỀ CHO PARENT COMPONENT SỬ DỤNG*/
+
+      this.data.list = list ;
+      this.props.onDataChange(this.data);
+
+      /* RE RENDER : ON DATA CHANGE THÀNH CÔNG */
+      this.onStateChange({
+        status:'done'
+      })
+    }
+
+    /* COMPONENT NÀY DÙNG MODAL : INTERAC INSIDE  */
+    getInfo(id){
+
+      return this.data.list.find(item=> item.id == id);
+
+
+    }
+
 
     onGridReady(params){
       /*this.gridApi = params.api;
@@ -126,55 +151,60 @@ class User extends Component{
     render(){
 
         const list = this.state.data ;
-
         const modalTitle = this.state.action ==='create' ? 'Tạo ': 'Cập nhật ';
 
-
-
         return(
-            <div hidden={ this.state.tab === 'user' ? false : true } >
-                <Modal isOpen={this.state.modal.status} fade={false}   toggle={this.toggleModal} >
-                   <ModalHeader toggle={this.toggle}> <i className="fa fa-plus"></i> { modalTitle+ this.state.name }  </ModalHeader>
-                   <ModalBody>
-                      <Row>
-                          <Col md="12">
-                            <div className="form-group">
-                              <label>Tên</label>
-                              <input className="form-control" id="name" onChange={ ()=>{ alert('asdasd') }} value="adasd"  type="text" placeholder="Nhập tên"/>
-                            </div>
-                          </Col>
-                      </Row>
-                   </ModalBody>
-                   <ModalFooter>
-                      <div role="group" className="btn-group">
-                          <Button className="btn-ubuntu" onClick={ this.toggleModal }> <i className="fa fa fa-reply"></i> Từ Chối  </Button>
-                          <Button className="btn-ubuntu-ok"> <i className="fa fa-chevron-circle-right"></i> Đồng Ý </Button>
-                      </div>
+            <div hidden={  this.state.onTab === this.code ? false : true } >
+              <div className="ag-theme-balham" id="myGrid" style={{boxSizing: "border-box", height: '74vh',width: '100%' }}>
 
-                   </ModalFooter>
-                 </Modal>
+                    <AgGridReact
 
-                 <div className="ag-theme-balham" id="myGrid" style={{boxSizing: "border-box", height: '74vh',width: '100%' }}>
+                        onGridReady={this.onGridReady.bind(this)}
+                        enableSorting={true}
 
-                       <AgGridReact
-
-                           onGridReady={this.onGridReady.bind(this)}
-                           enableSorting={true}
-
-                           enableColResize={true}
-                           rowSelection="multiple"
-                           columnDefs={this.state.columnDefs}
-                           rowData={this.state.rowData}>
-                       </AgGridReact>
-                  </div>
+                        enableColResize={true}
+                        rowSelection="multiple"
+                        columnDefs={this.table.columnDefs}
+                        rowData={this.data.list}>
+                    </AgGridReact>
+               </div>
             </div>
         )
     }
 
+    loadUser(){
+      const _this = this ;
+
+      this.model.get((res)=>{
+
+        const list = res.rows;
+        _this.onDataChange(list);
+
+      },(err)=>{
+        _this.hook.err(err)
+      });
+    }
+    componentDidMount(){
+      this.loadUser();
+    }
     componentWillReceiveProps(newProps){
 
+        /* nhận lện có liên quan đến tab : office */
+        if(newProps.onTab===this.code){
 
-        this.onAction(newProps);
+          Object.assign(this.state,newProps);
+
+          if(newProps.tabAction==='create'){
+              this.modal.open('post');
+          }
+        }else{
+
+          /* không liên quan => change tab*/
+          this.setState({
+            onTab:newProps.onTab
+          })
+        }
+
     }
 
 
