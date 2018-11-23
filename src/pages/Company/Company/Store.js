d3eef77736c19ade3fe1a1d600da9ede33eff2c9
Hồng Hải <honghai.js6@gmail.com>
2018-11-23 17:53:26 +0700

    Update Store.js

--- src/pages/Company/Company/Store.js
+++ src/pages/Company/Company/Store.js
@@ -1,171 +1,243 @@
 
 import React, {Component} from 'react';
 
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Row, Col, Label } from 'reactstrap';
+import {  Row, Col } from 'reactstrap';
 
 
-function BlockItem(props){
 
-  const data = props.data;
-  return(
-    <Col md="3" className="file-box">
-        <div className="file file-store" >
+import Model from '../../../config/model';
+import Hook from '../../../config/hook.class';
 
-              <div className="block">
-                <i className="fa fa-map-pin " style={{marginRight:5}}></i> {data.name} <br/>
-                Nhân viên : {data.num}
-              </div>
-              <div className="file-name">
-                <i className="fa fa-map-marker"></i> { data.address}
-                <br/>
-                <span>Added: Jan 11, 2016</span>
-              </div>
+import StoreClass from './store.class';
+import StoreForm from './store.form';
 
-        </div>
-    </Col>
-  )
-}
+
+
+
+
+import moment from 'moment';
+import 'moment/locale/vi';
+
 
 class Store extends Component{
 
     constructor(props){
       super(props);
 
-      this.state = {
-        tab:props.tab,
-        action:'',
+      this.code = 'store';
+      this.name = 'Cửa hàng';
 
-        name:'Cửa hàng ',
-        modal:{
-          status:false
-        },
-
-        data:[
-          {
-            id:1,
-            code:'code',
-            name:'asdasd',
-            address:'asdasdasd',
-            num:2
-          },
-          {
-            id:2,
-            code:'code2',
-            name:'Chi nhánh ABC',
-            address:'12 Trần hưng đạo, HCM',
-            num:5
-          },
-          {
-            id:3,
-            code:'code3',
-            name:'Chi nhánh ABC',
-            address:'12 Nguyễn Chí Thanh, HCM',
-            num:2
-          },
-          {
-            id:4,
-            code:'code3',
-            name:'Chi nhánh BCD',
-            address:'12 Nguyễn Chí Thanh, HCM',
-            num:2
-          },
-        ]
+      this.data = {
+        id:0,
+        list:[]
       }
 
-      this.toggleModal = this.toggleModal.bind(this)
+      this.state = {
+        onAction:'',
+        status:'',
+
+        onTab:props.onTab
+      }
+
+      this.setup();
     }
 
-    toggleModal(){
-      this.setState({
-        modal:{
-          status:!this.state.modal.status
-        },
-        action:''
+    setup(){
+      this.model = new Model('stores');
+      this.hook = new Hook(this);
 
-      });
-
-
+      this.modal = new StoreClass(this);
     }
-    onAction(newProps){
 
+    setData(name,value){
+      this.data[name] = value;
 
-
-      let active = newProps.tab ==='store' ? newProps.tabAction ==='create' ? true: false : false ;
-
-
-      this.setState({
-        tab:newProps.tab,
-        action:newProps.tabAction,
-        modal:{
-          status:active
-        }
-
+      /* RE RENDER : ON DATA CHANGE THÀNH CÔNG */
+      this.onStateChange({
+        status:'done'
       })
 
+    }
+
+    onStateChange(newState){
+
+      /* KEEP PRIVATE DATA*/
+      this.setState(Object.assign(this.state,newState));
+
+      /* trả giá tri về cho parent component sử dụng */
+      this.props.onStateChange(this.state);
 
 
     }
-    render(){
 
-        const list = this.state.data ;
+    onDataChange(list){
 
-        const modalTitle = this.state.action ==='create' ? 'Tạo ': 'Cập nhật ';
+      /* TRẢ GIÁ TRỊ VỀ CHO PARENT COMPONENT SỬ DỤNG*/
+
+      this.data.list = list ;
+      this.props.onDataChange(this.data);
+
+      /* RE RENDER : ON DATA CHANGE THÀNH CÔNG */
+      this.onStateChange({
+        status:'done'
+      })
+    }
+
+    /* COMPONENT NÀY DÙNG MODAL : INTERAC INSIDE  */
+    getInfo(id){
+
+      return this.data.list.find(item=> item.id == id);
+
+    }
+
+    BlockItem(props){
+      const data = props.data;
+
+      const date = moment(data.date_created).format('YYYY-MM-DD HH:mm:ss');
 
 
+      return(
+        <Col md="3" key={ Math.random() } className="file-box">
+            <div className="file" >
 
-        return(
-            <div hidden={ this.state.tab === 'store' ? false : true } >
-                <Modal isOpen={this.state.modal.status} fade={false}   toggle={this.toggleModal} >
-                   <ModalHeader toggle={this.toggle}> <i className="fa fa-plus"></i> { modalTitle+ this.state.name }  </ModalHeader>
-                   <ModalBody>
-                      <Row>
-                          <Col md="12">
-                            <div className="form-group">
-                              <label>Tên</label>
-                              <input className="form-control" id="name" onChange={ ()=>{ alert('asdasd') }} value="adasd"  type="text" placeholder="Nhập tên"/>
-                            </div>
-                          </Col>
-                      </Row>
-                   </ModalBody>
-                   <ModalFooter>
-                      <div role="group" className="btn-group">
-                          <Button className="btn-ubuntu" onClick={ this.toggleModal }> <i className="fa fa fa-reply"></i> Từ Chối  </Button>
-                          <Button className="btn-ubuntu-ok"> <i className="fa fa-chevron-circle-right"></i> Đồng Ý </Button>
-                      </div>
+                  <div className="block">
+                    <div>
+                       <span><i className="fa fa-map-pin mr-5"></i> {data.name}</span>
+                       <span className="pull-right">
+                         <a className='pointer' onClick={ ()=>{ this.modal.open('put',data) } }> <i className="fa fa-gear"></i> </a>
+                       </span>
+                    </div>
+                    <i className="fa fa-phone mr-5"></i> { data.phone === null ? 'n/a' : data.phone } <br/>
 
-                   </ModalFooter>
-                 </Modal>
-
-                 <Row>
-                   {
-                     list.map((item)=>{
-                       return(
-                         <BlockItem key={item.id} data={item} />
-                       )
-                     })
-                   }
-                 </Row>
+                  </div>
+                  <div className="file-name">
+                    <i className="fa fa-map-marker mr-5"></i> { data.address.substring(0,30) }
+                    <br/>
+                    <span> {   moment(date).fromNow() } </span>
+                  </div>
 
             </div>
-        )
+        </Col>
+      )
     }
 
+
+    loading(){
+      this.onStateChange(Object.assign(this.state,{
+        onAction:'read',
+        status:'loading'
+      }));
+    }
+
+
+    loadStore(){
+      const _this = this ;
+      this.loading();
+
+      this.model.get((res)=>{
+
+        const list = res.rows;
+
+        _this.onDataChange(list);
+
+      },(err)=>{
+        _this.hook.err(err)
+      });
+
+    }
+
+    componentDidMount(){
+
+
+      this.loadStore();
+      this.modal.loadCityList();
+      this.modal.loadDistrictList(this.modal.form.region_code,()=>{}); // code : tp ho chi minh
+
+    }
+
+    /* NHẬN LỆNH TỪ NEW PROPS : PHÂN TỪ DATA VÀ ACTION*/
+    receiveAction(newProps){
+
+
+      /* nhận lện có liên quan đến tab : office */
+      if(newProps.onTab===this.code){
+
+        Object.assign(this.state,newProps);
+
+        if(newProps.tabAction==='create'){
+            this.modal.open('post');
+        }
+      }else{
+
+        /* không liên quan => change tab*/
+
+        this.setState({
+          onTab:newProps.onTab
+        })
+      }
+
+    }
+
+    /* NHẬN lệnh : từ NEW PROPS TỪ BODY OBJECT*/
     componentWillReceiveProps(newProps){
 
 
-        this.onAction(newProps);
+        this.receiveAction(newProps);
+
+
+
     }
 
-
-
-
-
+    /* TRIGGER AFFTER SOMETHING*/
     componentDidUpdate(prevProps, prevState){
+
+      //alert(JSON.stringifyr);
+      //alert('componentDidUpdate: '+ JSON.stringify(prevProps));
+      //alert('componentDidUpdate');
       /*alert('ok man : componentDidUpdate');
       alert(JSON.stringify(prevProps));
       alert(JSON.stringify(prevState));*/
 
     }
+
+    /* DESTROY - REMOVE SOMETHING*/
+    componentWillUnmount(){
+      alert('componentWillUnmount happen: store');
+    }
+
+
+
+    render(){
+
+        const list = this.data.list ;
+        const modalTitle = this.state.onAction ==='post' ? 'Tạo ': 'Cập nhật ';
+
+        const { form } =  this.modal;
+
+
+        return(
+            <div hidden={  this.state.onTab === this.code ? false : true } >
+
+
+                 <StoreForm name={ modalTitle+' '+ this.name} onAction={ this.state.onAction} modal={ this.modal } />
+
+                 <Row>
+
+                    {
+                      list.map((item)=>{
+
+
+                        return this.BlockItem({key:item.id, data:item});
+
+                      })
+                    }
+
+                  </Row>
+            </div>
+        )
+    }
+
+
 
 }
 
