16771135ec3331f166d38d3bcc418577780117fb
honghaijs6 <44365604+honghaijs6@users.noreply.github.com>
2018-11-28 03:12:01 +0700

    Update department.form.js

--- src/pages/Company/Company/department.form.js
+++ src/pages/Company/Company/department.form.js
@@ -1,10 +1,10 @@
 
 import React, { Component } from 'react';
 
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Row, Col, Label,
-    Popover, PopoverHeader, PopoverBody
+import {  Row, Col
 
 } from 'reactstrap';
+
 
 
 import BenModal from '../../../components/BenModal';
@@ -14,15 +14,15 @@ function FrmR1(props){
   const modal = props.modal ;
   return(
     <Row>
-        <Col md="12">
+        <Col md="12"> 
 
           <div className="form-group">
             <label>Mã </label>
-            <input  className="form-control" id="code" onChange={(e)=>{ modal.data.code = e.target.value.trim() }} defaultValue={ modal.data.code }   type="text" placeholder="Nhập mã"/>
+            <input  className="form-control" id="code" onChange={(e)=>{ modal.form.code = e.target.value.trim() }} defaultValue={ modal.form.code }   type="text" placeholder="Nhập mã"/>
           </div>
           <div className="form-group">
             <label>Tên  </label>
-            <input onChange={(e)=>{ modal.data.name = e.target.value.trim() }}  className="form-control" id="name" defaultValue={ modal.data.name }   type="text" placeholder="Nhập tên"/>
+            <input onChange={(e)=>{ modal.form.name = e.target.value.trim() }}  className="form-control" id="name" defaultValue={ modal.form.name }   type="text" placeholder="Nhập tên"/>
           </div>
         </Col>
     </Row>
