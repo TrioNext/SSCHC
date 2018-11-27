d881b549f2818598852837b7690dd130e3622534
Hồng Hải <honghai.js6@gmail.com>
2018-11-27 17:52:58 +0700

    Update BenModal.js

--- src/components/BenModal.js
+++ src/components/BenModal.js
@@ -7,9 +7,10 @@ import { Button, Modal, ModalHeader, ModalBody, Form,
 
 /*
 BenModal : props
-  - state :  rule state : { onAction, status}
+  - onAction :  rule state : { onAction, status}
   - modal : it is a controller class
   - name : string
+
   refErr : React.createRef  object
 */
 
@@ -20,18 +21,15 @@ class BenModal extends Component{
 
     super(props)
 
-    this.state = props.state;
-
-    this.modal = props.modal;
-    this.name = props.name;
-    this.refErr = props.refErr;
-
-
   }
+
   render(){
+
+
+
     return(
-      <Modal  size="lg" isOpen={ this.modal.active } fade={false}   toggle={ ()=>{  this.modal.toggle() } } >
-        <ModalHeader toggle={ ()=>{ this.modal.toggle() } }> <i className="fa fa-plus"></i> { this.name }  </ModalHeader>
+      <Modal  size="lg" isOpen={ this.props.modal.active } fade={false}   toggle={ ()=>{  this.props.modal.toggle() } } >
+        <ModalHeader toggle={ ()=>{ this.props.modal.toggle() } }> <i className="fa fa-plus"></i> { this.props.name }  </ModalHeader>
 
         <ModalBody style={{
             padding:30
@@ -44,8 +42,8 @@ class BenModal extends Component{
         <div className="my-modal-footer">
            <div className="float-right">
                <div role="group" className="btn-group">
-                     <Button className="btn-ubuntu" onClick={ ()=>{ this.modal.toggle() } }> <i className="fa fa fa-reply"></i> Từ Chối  </Button>
-                     <Button className="btn-ubuntu-ok" onClick={ ()=>{ this.modal.onSubmit() } }> <i className="fa fa-chevron-circle-right"></i> Đồng Ý </Button>
+                     <Button className="btn-ubuntu" onClick={ ()=>{ this.props.modal.toggle() } }> <i className="fa fa fa-reply"></i> Từ Chối  </Button>
+                     <Button className="btn-ubuntu-ok" onClick={ ()=>{ this.props.modal.onSubmit() } }> <i className="fa fa-chevron-circle-right"></i> Đồng Ý </Button>
                </div>
 
            </div>
@@ -53,19 +51,19 @@ class BenModal extends Component{
         </div>
 
         <div className="modal-err " >
-           <div className="float-left form-err text-muted" id="form-err" ref={ this.refErr }>
+           <div className="float-left form-err text-muted" id="form-err">
                status
            </div>
            <div className="float-right">
-             <a id="btnDel" hidden={ this.state.onAction === 'post' ? true : false  } className={'text-muted btn-delete ' } onClick={ ()=>{ this.modal.popover.toggle() } }>
+             <a id="btnDel" hidden={ this.props.onAction === 'post' ? true : false  } className={'text-muted btn-delete ' } onClick={ ()=>{ this.props.modal.popover.toggle() } }>
                <i className="fa fa-trash"></i> Xoá
              </a>
-             <Popover placement="bottom" isOpen={this.modal.popover.active } target="btnDel"  toggle={ ()=>{ this.modal.popover.toggle() } }>
+             <Popover placement="bottom" isOpen={this.props.modal.popover.active } target="btnDel"  toggle={ ()=>{ this.props.modal.popover.toggle() } }>
                <PopoverHeader>Bạn có chắc chắn không?</PopoverHeader>
                <PopoverBody className="text-center pa-15">
-                 <button onClick={ ()=>{  this.modal.popover.btnYes() } } className="btn btn-sm btn-success mr-20">Có</button>
+                 <button onClick={ ()=>{  this.props.modal.popover.btnYes() } } className="btn btn-sm btn-success mr-20">Có</button>
 
-                 <button onClick={ ()=>{  this.modal.popover.toggle() } } className="btn btn-sm btn-secondary">Không</button>
+                 <button onClick={ ()=>{  this.props.modal.popover.toggle() } } className="btn btn-sm btn-secondary">Không</button>
                </PopoverBody>
              </Popover>
            </div>
