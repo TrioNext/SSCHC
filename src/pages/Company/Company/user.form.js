4310c2667f54241f1258f8a76df06e27c96526ab
Hồng Hải <honghai.js6@gmail.com>
2018-11-27 17:53:25 +0700

    Update user.form.js

--- src/pages/Company/Company/user.form.js
+++ src/pages/Company/Company/user.form.js
@@ -67,6 +67,12 @@ function FrmR3(props){
 
   const modal = props.modal;
   const { form } = props.modal ;
+
+  let list = [];
+  props.jobtype.map((item,index)=>{
+    list.push(<option value={item} key={index} id={ index} > { item } </option>)
+  })
+
   return (
     <Row form>
 
@@ -80,7 +86,10 @@ function FrmR3(props){
       <Col md={6}>
         <FormGroup>
           <Label> Loại hình công việc <span className="text-danger">*</span></Label>
-          <Input type="text" onChange={ (e)=>{ modal.onChange('phone', e);  } } defaultValue={ modal.form.phone }  placeholder="nhập số ĐT" />
+          <Input type="select">
+              { list }
+          </Input>
+
         </FormGroup>
       </Col>
 
@@ -93,6 +102,12 @@ function FrmR4(props){
 
   const modal = props.modal;
   const { form } = props.modal ;
+
+  let list = [];
+  props.joblevel.map((item,index)=>{
+    list.push(<option value={item} key={index} id={ index} > { item } </option>)
+  })
+
   return (
     <Row form>
 
@@ -106,7 +121,9 @@ function FrmR4(props){
       <Col md={6}>
         <FormGroup>
           <Label> Cấp bậc <span className="text-danger">*</span></Label>
-          <Input type="text" onChange={ (e)=>{ modal.onChange('phone', e);  } } defaultValue={ modal.form.phone }  placeholder="nhập số ĐT" />
+          <Input type="select">
+            {list}
+          </Input>
         </FormGroup>
       </Col>
 
@@ -148,49 +165,52 @@ class UserForm extends Component{
     super(props);
 
     this.state = {
-      onAction:this.props.onAction
+      onAction:props.onAction
     }
 
-    this.modal = props.modal;
-    this.name = props.name;
+
+    this.job_type = [
+      'Nhân viên chính thức',
+      'Bán thời gian',
+      'Thử việc',
+      'Làm thêm ngoài giờ',
+      'Nhân viên thời vụ',
+      'Làm dự án'
+    ];
+
+    this.job_level = [
+      'Mới tốt nghiệp/ Thực tập',
+      'Nhân viên',
+      'Trưởng nhóm/ Giám sát',
+      'Phó phòng',
+      'Trưởng phòng',
+      'Phó cửa hàng',
+      'Trưởng cửa hàng',
+      'Phó giám đốc',
+      'Giám đốc',
+      'Giám đốc điều hành',
+      'Phó chủ tịch',
+      'Chủ tịch'
+    ];
 
 
 
   }
-
-
-  componentWillReceiveProps(newProps){
-
-      this.modal = newProps.modal;
-
-      this.setState({
-        onAction:newProps.onAction
-      });
-
-
-  }
-
 
 
 
   render(){
 
-    const modalTitle = this.state.onAction ==='post' ? 'Tạo ': 'Cập nhật ';
-    
-
 
     return (
 
-      <BenModal name={ modalTitle + this.name } state={ this.state} modal={ this.modal }  >
+      <BenModal name={ this.props.name } onAction={ this.props.onAction } modal={ this.props.modal }  >
 
-        <FrmR1 modal={ this.modal }  />
-        <FrmR2 modal={ this.modal }  />
-        <FrmR3 modal={ this.modal }  />
-        <FrmR4 modal={ this.modal }  />
-        <FrmR5 modal={ this.modal }  />
-
-
-
+        <FrmR1 modal={ this.props.modal }  />
+        <FrmR2 modal={ this.props.modal }  />
+        <FrmR3 modal={ this.props.modal } jobtype={ this.job_type }  />
+        <FrmR4 modal={ this.props.modal } joblevel={ this.job_level }  />
+        <FrmR5 modal={ this.props.modal }  />
 
       </BenModal>
 
