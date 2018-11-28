4fd62015263adf078ee4f52931e6a02d82d10e0c
Hồng Hải <honghai.js6@gmail.com>
2018-11-28 17:51:31 +0700

    Create StoreModalComp.js

---
+++ src/pages/Company/Company/StoreModalComp.js
@@ -0,0 +1,117 @@
+import React, {Component} from 'react';
+
+import {  Row, Col, Label,  Form, FormGroup,FormText, Input } from 'reactstrap';
+
+import moment from 'moment';
+
+import BenModal from '../../../components/BenModal';
+import SelectCity from '../../../components/SelectCity';
+import SelectDist from '../../../components/SelectDist';
+
+
+function FrmR1(props){
+
+  const modal = props.modal;
+  return (
+    <Row form>
+      <Col md={4}>
+        <FormGroup>
+          <Label> Mã <span className="text-danger">*</span></Label>
+          <Input type="text" onChange={ (e)=>{ modal.onChange('code', e);  } } defaultValue={ modal.form.code }  placeholder="Tạo mã" />
+        </FormGroup>
+      </Col>
+      <Col md={8}>
+        <FormGroup>
+          <Label> Tên <span className="text-danger">*</span></Label>
+          <Input type="text" onChange={ (e)=>{ modal.onChange('name', e);  } } defaultValue={ modal.form.name }  placeholder="Nhập tên" />
+        </FormGroup>
+      </Col>
+    </Row>
+  )
+}
+
+
+
+function FrmR2(props){
+
+
+  const modal = props.modal ;
+  const form = props.modal.form;
+
+  return(
+    <Row form>
+      <Col md={4}>
+        <FormGroup>
+          <Label> Số ĐT <span className="text-danger">*</span></Label>
+          <Input type="text" onChange={ (e)=>{ modal.onChange('phone', e);  } } defaultValue={ modal.form.phone }  placeholder="nhập số ĐT" />
+        </FormGroup>
+      </Col>
+      <Col md={4}>
+        <FormGroup>
+          <Label> Tỉnh / Thành </Label>
+            <SelectCity modal={ modal } selected={ form.region_code} />
+        </FormGroup>
+      </Col>
+      <Col md={4}>
+        <FormGroup>
+          <Label> Quận/Huyện </Label>
+
+            <SelectDist modal={ modal } selected={ form.subregion_code } />
+        </FormGroup>
+      </Col>
+    </Row>
+  )
+}
+
+function FrmR3(props){
+
+  const modal = props.modal;
+  const { form } = props.modal ;
+  return (
+    <div>
+      <FormGroup>
+        <Label>Địa chỉ <span className="text-danger">*</span></Label>
+          <Input type="text" onChange={ (e)=>{ modal.onChange('address', e);  } } defaultValue = { form.address } placeholder="Nhập địa chỉ"/>
+      </FormGroup>
+
+      <FormGroup>
+        <Label>IP được chấm công</Label>
+          <Input type="text" onChange={ (e)=>{ modal.onChange('ip_chamcong', e);  } } defaultValue = { form.ip_chamcong }  placeholder="Nhập địa chỉ IP"/>
+      </FormGroup>
+    </div>
+  )
+}
+
+
+
+class StoreForm extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      onAction:this.props.onAction
+    }
+
+  }
+
+
+  render(){
+
+    return (
+
+      <BenModal name={ this.props.name } onAction={ this.props.onAction } modal={ this.props.modal }  >
+
+        <FrmR1 modal={ this.props.modal }  />
+        <FrmR2 modal={ this.props.modal }  />
+        <FrmR3 modal={ this.props.modal }  />
+
+
+      </BenModal>
+
+
+    )
+  }
+}
+
+export default StoreForm
