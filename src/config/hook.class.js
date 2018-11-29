95e926b46e7fcb0acfeba733336111e886724df2
Hồng Hải <honghai.js6@gmail.com>
2018-11-29 17:53:25 +0700

    Update hook.class.js

--- src/config/hook.class.js
+++ src/config/hook.class.js
@@ -22,42 +22,13 @@ class Hook {
       switch(onAction){
         case 'post':
 
-            if(idata.name==='success'){
-
-
-              const list = this.app.data.list;
-              list.unshift(idata.data);
-
-              this.app.onDataChange(list);
-              this.app.modal.toggle();
-
-            }else{
-
-                this.showErr(idata.message);
-
-            }
-
 
         break;
         case 'put':
 
             if(idata.name==='success'){
 
-              const {id} = idata.condition.where;
-              const list = this.app.data.list;
 
-              Object.assign(this.app.modal.form,{id:id});
-
-              list.map((item,index)=>{
-
-                if(parseInt(item.id) == parseInt(id)){
-                   list[index] = this.app.modal.form;
-                }
-              });
-
-              
-              this.app.onDataChange(list);
-              this.app.modal.toggle();
 
             }else{ this.showErr(idata.message); }
 
@@ -66,16 +37,7 @@ class Hook {
 
         case 'delete':
 
-            if(idata.name==='success'){
 
-               let list = this.app.data.list.filter((item)=>{
-                 return parseInt(item.id) !== parseInt(idata.id);
-               });
-
-               this.app.onDataChange(list);
-               this.app.modal.toggle();
-
-            }else{ this.showErr(idata.message); }
 
         break;
 
@@ -87,29 +49,12 @@ class Hook {
   error(err){
 
 
-      const data = err.response.data ;
-      const msg = data.errors[0];
 
-      this.showErr(msg);
 
   }
   showErr(msg){
 
-      if(typeof msg === 'object'){
-        msg = msg.message.indexOf('must be unique') >-1 ? 'Mã này đã được dùng' : msg.message ;
-      }
 
-      let el = document.getElementById('form-err');
-
-      if(typeof el ==='object'){
-        el.innerHTML = msg;
-        setTimeout(()=>{
-          el.innerHTML = 'status';
-        },2000)
-      }else{
-        alert('Đã xãy ra lỗi')
-        console.log(msg);
-      }
 
   }
 }
