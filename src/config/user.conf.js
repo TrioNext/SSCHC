559c64565ab948cb096e4b668e8d102f70b9189a
Hồng Hải <honghai.js6@gmail.com>
2018-11-30 17:52:15 +0700

    Create user.conf.js

---
+++ src/config/user.conf.js
@@ -0,0 +1,27 @@
+export default {
+
+  job_type:[
+    'Nhân viên chính thức',
+    'Bán thời gian',
+    'Thử việc',
+    'Làm thêm ngoài giờ',
+    'Nhân viên thời vụ',
+    'Làm dự án'
+  ],
+  job_level:[
+    'Mới tốt nghiệp',
+    'Thực tập',
+    'Nhân viên',
+    'Trưởng nhóm/ Giám sát',
+    'Phó phòng',
+    'Trưởng phòng',
+    'Phó cửa hàng',
+    'Trưởng cửa hàng',
+    'Phó giám đốc',
+    'Giám đốc',
+    'Giám đốc điều hành',
+    'Phó chủ tịch',
+    'Chủ tịch'
+  ]
+
+}
