55272d6656e0d2b3c14cea85d06e28849238b5f0
Hồng Hải <honghai.js6@gmail.com>
2018-12-10 17:50:52 +0700

    Create department.store.js

---
+++ src/store/department.store.js
@@ -0,0 +1,18 @@
+
+//import { observable, action } from 'mobx';
+
+
+
+class DepartmentStore {
+
+  data = [];
+
+  static get(){
+
+    return this.data ;
+  }
+}
+
+export default function(){
+    return []
+}
