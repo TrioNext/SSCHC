2b076655bb025cca3108aa4c95cd0fcdcc8e5d5e
Hồng Hải <honghai.js6@gmail.com>
2018-12-11 18:20:09 +0700

    Update reducer-department.js

--- src/reducers/reducer-department.js
+++ src/reducers/reducer-department.js
@@ -1,4 +1,57 @@
 
-export default function(){
-    return []
+/*
+action = {
+  type:'ACTION_TYPE',
+  model:Model,
+  data:{}
+  id:0
 }
+*/
+
+export default function(state = [],action = {}){
+  switch(action.type){
+
+
+    case 'FETCH':
+        alert('fetch data');
+
+        console.log(action.model);
+
+        return state;
+    break;
+    case 'SET':
+
+      state = action.list ;
+      return state ;
+
+    break ;
+
+    case 'GET':
+
+       let ret = {};
+      state.map((item)=>{
+        if(parseInt(item)===action.id){
+          ret = item;
+        }
+      });
+
+      return ret ;
+    break;
+
+    case 'POST':
+      return state ;
+    break;
+
+    case 'PUT':
+
+    break;
+
+    case 'DELETE':
+
+    break ;
+
+    default:
+      return state;
+
+  }
+};
