7605e50bbaf7c596272eb20b48ebea2112e5fa8e
Hồng Hải <honghai.js6@gmail.com>
2018-12-19 18:11:02 +0700

    Create beforePost.js

---
+++ src/hook/beforePost.js
@@ -0,0 +1,25 @@
+
+export default function(fields=[],form={}){
+
+  let ret = '' ;
+
+  if(fields.length>0){
+
+    fields = fields.reverse();
+
+    Object.keys(form).map((item)=>{
+      fields.map((item2)=>{
+        if(form[item2] === '' ||  form[item2] === 0){
+          ret = 'vui lòng kiểm tra thông tin ' ;
+          document.getElementById(item2).focus();
+
+        }
+
+      });
+    });
+  }
+
+  let el = document.getElementById('form-err');
+  el.innerHTML = ret;
+  return ret ;
+}
