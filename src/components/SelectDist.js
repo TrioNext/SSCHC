22ce236ab72993938841f406623b52cd6f66a9d6
Hồng Hải <honghai.js6@gmail.com>
2018-12-21 18:12:25 +0700

    Update SelectDist.js

--- src/components/SelectDist.js
+++ src/components/SelectDist.js
@@ -17,7 +17,7 @@ function SelectDist(props){
   const modal = props.modal ;
 
   let list = [];
-  modal.listDistrict.map((item)=>{
+  modal.subregions.map((item)=>{
     list.push(<option id={item.id} key={item.id} value={ item.code } > { item.name_with_type } </option>)
   })
 
