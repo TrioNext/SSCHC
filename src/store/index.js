c57f46bc29884f342d2696e9b9f7b03474322f38
Hồng Hải <honghai.js6@gmail.com>
2018-12-21 18:13:27 +0700

    Delete index.js

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-
-import { combineReducers } from 'redux';
-import departmentStore from './department.store';
-
-const allReducers = combineReducers({
-  department:departmentStore
-});
-
-export default allReducers;
