a5fd2f63785cff46bbc5d90428b6c09ae12e12a0
Hồng Hải <honghai.js6@gmail.com>
2018-12-21 18:13:06 +0700

    Update index.js

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,20 @@
 import { combineReducers } from 'redux';
 import reducerDepartment from './reducer-department';
 import reducerUser from './reducer-user';
+import reducerOffice from './reducer-office';
+import reducerRegion from './reducer-region';
+import reducerSubregion from './reducer-subregion';
+
+
 
 
 
 const allReducers = combineReducers({
   department:reducerDepartment,
-  user:reducerUser
+  user:reducerUser,
+  office:reducerOffice,
+  region:reducerRegion,
+  subregion:reducerSubregion
 
 });
 
