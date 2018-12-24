6ea2c89e7b3777a292a962fb2b4b4ae742d049c2
honghaijs6 <44365604+honghaijs6@users.noreply.github.com>
2018-12-24 07:22:16 +0700

    Update afterPost.js

--- src/hook/afterPost.js
+++ src/hook/afterPost.js
@@ -4,3 +4,44 @@
   1. socket activities update channel room :  using socket class intances object
   2. component re-render : using class intance object
 */
+
+import userConf from '../config/user.conf';
+
+
+
+export const onSubmitAndCloseModal = (res,_this)=>{
+
+  if(typeof res.name  !== 'undefined'){
+    const status = res.name ;
+    if(status==='success'){
+      //_this.app.onStateChange({onAction:_this.state.onAction,status:'success'});
+      _this.toggle();
+
+      if(_this.state.onAction==='post'){
+        emptyForm(_this.form);
+      }
+
+    }
+  }
+}
+
+export const emptyForm = (form)=>{
+
+  Object.keys(form).map((item)=>{
+    if(typeof form[item] ==='string'){
+      form[item] = '';
+    }
+  });
+}
+
+export const emptyFormUser = (form={})=>{
+  Object.keys(form).map((item)=>{
+    if(typeof form[item] ==='string'){
+      form[item] = '';
+    }
+  });
+
+  form.password = userConf.defaultPass; /* reset defaultPass*/
+
+
+}
