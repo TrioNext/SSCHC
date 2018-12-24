346039c7d6005801eb314daa51a75870f371fb28
honghaijs6 <44365604+honghaijs6@users.noreply.github.com>
2018-12-24 07:22:31 +0700

    Update DepModalComp.js

--- src/pages/Company/Department/DepModalComp.js
+++ src/pages/Company/Department/DepModalComp.js
@@ -12,17 +12,19 @@ import BenModal from '../../../components/BenModal';
 function FrmR1(props){
 
   const modal = props.modal ;
+  const data = modal.data;
+
   return(
     <Row>
         <Col md="12">
 
           <div className="form-group">
             <label>Mã </label>
-            <input  className="form-control" id="code" onChange={(e)=>{ modal.form.code = e.target.value.trim() }} defaultValue={ modal.form.code }   type="text" placeholder="Nhập mã"/>
+            <input  className="form-control" id="code" onChange={(e)=>{ modal.onChange('code',e) }} defaultValue={ data.code }   type="text" placeholder="Nhập mã"/>
           </div>
           <div className="form-group">
             <label>Tên  </label>
-            <input onChange={(e)=>{ modal.form.name = e.target.value.trim() }}  className="form-control" id="name" defaultValue={ modal.form.name }   type="text" placeholder="Nhập tên"/>
+            <input onChange={(e)=>{ modal.onChange('name',e) }}  className="form-control" id="name" defaultValue={ data.name }   type="text" placeholder="Nhập tên"/>
           </div>
         </Col>
     </Row>
