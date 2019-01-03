c7d77ae099d2849d79c57b934c2cafabd7403c18
honghaijs6 <44365604+honghaijs6@users.noreply.github.com>
2019-01-03 07:09:30 +0700

    Update SelectDist.js

--- src/components/SelectDist.js
+++ src/components/SelectDist.js
@@ -17,9 +17,11 @@ function SelectDist(props){
   const modal = props.modal ;
 
   let list = [];
-  /*modal.subregions.map((item)=>{
+
+
+  props.subregions.map((item)=>{
     list.push(<option id={item.id} key={item.id} value={ item.code } > { item.name_with_type } </option>)
-  })*/
+  })
 
   return(
     <Input onChange={ (e)=>{  modal.onChangeDist(e)  } }  type="select" defaultValue={ props.selected }>
