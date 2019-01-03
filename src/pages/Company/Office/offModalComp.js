b3108812ae444dcb4e418924e545c761f135b340
Hồng Hải <honghai.js6@gmail.com>
2019-01-03 17:58:33 +0700

    Update offModalComp.js

--- src/pages/Company/Office/offModalComp.js
+++ src/pages/Company/Office/offModalComp.js
@@ -14,21 +14,23 @@ import SelectMinute from '../../../components/SelectMinute';
 function FrmR1(props){
 
   const modal = props.modal;
-
+  const form = modal.form ;
   const data = modal.data ;
+
+  const defaultValue = props.onAction === 'post' ? '' : form.code;
 
   return (
     <Row form>
       <Col md={4}>
         <FormGroup>
           <Label id="la-code"> Mã văn phòng <span className="text-danger">*</span></Label>
-          <Input type="text" id="code" onChange={ (e)=>{ modal.onChange('code', e);  } } defaultValue={ data.code }  placeholder="Tạo mã" />
+          <Input type="text" id="code" onChange={ (e)=>{ modal.onChange('code', e);  } } defaultValue={ defaultValue }  placeholder="Tạo mã" />
         </FormGroup>
       </Col>
       <Col md={8}>
         <FormGroup>
           <Label id="la-name"> Tên văn phòng <span className="text-danger">*</span></Label>
-          <Input type="text" id="name" onChange={ (e)=>{ modal.onChange('name', e);  } } defaultValue={ data.name }  placeholder="Nhập tên" />
+          <Input type="text" id="name" onChange={ (e)=>{ modal.onChange('name', e);  } } defaultValue={ form.name }  placeholder="Nhập tên" />
         </FormGroup>
       </Col>
     </Row>
@@ -41,31 +43,28 @@ function FrmR2(props){
 
 
   const modal = props.modal ;
-  const form = props.modal.form;
-
   const data = modal.data ;
-
-
+  const form = modal.form ;
 
   return(
     <Row form>
       <Col md={4}>
         <FormGroup>
           <Label id="la-phone"> Số ĐT <span className="text-danger">*</span></Label>
-          <Input type="text" id="phone" onChange={ (e)=>{ modal.onChange('phone', e);  } } defaultValue={ data.phone }  placeholder="nhập số ĐT" />
+          <Input type="text" id="phone" onChange={ (e)=>{ modal.onChange('phone', e);  } } defaultValue={ form.phone }  placeholder="nhập số ĐT" />
         </FormGroup>
       </Col>
       <Col md={4}>
         <FormGroup>
           <Label> Tỉnh / Thành </Label>
-            <SelectCity modal={ modal } regions={ props.regions} selected={ data.region_code} />
+            <SelectCity modal={ modal } regions={ props.regions} selected={ form.region_code} />
         </FormGroup>
       </Col>
       <Col md={4}>
         <FormGroup>
           <Label> Quận/Huyện </Label>
 
-            <SelectDist modal={ modal } subregions={ props.subregions }  selected={ data.subregion_code } />
+            <SelectDist modal={ modal } subregions={ props.subregions }  selected={ form.subregion_code } />
         </FormGroup>
       </Col>
     </Row>
@@ -75,20 +74,19 @@ function FrmR2(props){
 function FrmR3(props){
 
   const modal = props.modal;
-  const { form } = props.modal ;
-
   const data = modal.data ;
+  const form = modal.form ;
 
   return (
     <div>
       <FormGroup>
         <Label id="la-address">Địa chỉ <span className="text-danger">*</span></Label>
-          <Input type="text" id="address" onChange={ (e)=>{ modal.onChange('address', e);  } } defaultValue = { data.address } placeholder="Nhập địa chỉ"/>
+          <Input type="text" id="address" onChange={ (e)=>{ modal.onChange('address', e);  } } defaultValue = { form.address } placeholder="Nhập địa chỉ"/>
       </FormGroup>
 
       <FormGroup>
         <Label>IP được chấm công</Label>
-          <Input type="text" onChange={ (e)=>{ modal.onChange('ip_chamcong', e);  } } defaultValue = { data.ip_chamcong }  placeholder="Nhập địa chỉ IP"/>
+          <Input type="text" onChange={ (e)=>{ modal.onChange('ip_chamcong', e);  } } defaultValue = { form.ip_chamcong }  placeholder="Nhập địa chỉ IP"/>
       </FormGroup>
     </div>
   )
@@ -98,9 +96,12 @@ function FrmR3(props){
 function FrmR4(props){
 
   const modal = props.modal ;
-  const { form } =  props.modal;
   const data = modal.data ;
-  const begin = moment('2018-11-20 '+data.working_begin).format('HH:mm').split(':');
+  const form = modal.form ;
+
+
+  const begin = moment('2018-11-20 '+form.working_begin).format('HH:mm').split(':');
+
 
 
   return (
@@ -109,13 +110,13 @@ function FrmR4(props){
         <FormGroup>
           <Label> Giờ làm việc </Label>
 
-            <SelectHour modal={ modal } selected={ Number(begin[0]) } />
+            <SelectHour modal={ modal } type="working_begin" selected={ Number(begin[0]) } />
         </FormGroup>
       </Col>
       <Col md={6}>
         <FormGroup>
           <Label> . </Label>
-            <SelectMinute modal={ modal } selected={ Number(begin[1]) } />
+            <SelectMinute modal={ modal } type="working_begin" selected={ Number(begin[1]) } />
         </FormGroup>
       </Col>
     </Row>
@@ -125,12 +126,10 @@ function FrmR4(props){
 function FrmR5(props){
 
   const modal = props.modal ;
-  const { form } =  props.modal;
-
   const data = modal.data ;
+  const form = modal.form ;
 
-
-  const end = moment('2018-11-20 '+data.working_end).format('HH:mm').split(':');
+  const end = moment('2018-11-20 '+form.working_end).format('HH:mm').split(':');
 
 
   return (
@@ -139,7 +138,7 @@ function FrmR5(props){
         <FormGroup>
           <Label> Giờ tan ca </Label>
 
-              <SelectHour modal={ modal } selected={  Number(end[0]) } />
+              <SelectHour modal={ modal } type="working_end" selected={  Number(end[0]) } />
 
         </FormGroup>
       </Col>
@@ -147,7 +146,7 @@ function FrmR5(props){
         <FormGroup>
           <Label> . </Label>
 
-            <SelectMinute modal={ modal } selected={ Number(end[1]) } />
+            <SelectMinute modal={ modal } type="working_end" selected={ Number(end[1]) } />
         </FormGroup>
       </Col>
     </Row>
@@ -174,15 +173,15 @@ class OffModalComp extends Component{
 
       <BenModal name={this.props.name} onAction={ this.props.onAction } modal={ this.props.modal }  >
 
-          <FrmR1 modal={ this.props.modal } />
+          <FrmR1 onAction={ this.props.onAction } modal={ this.props.modal } />
 
-          <FrmR2 regions={ this.props.regions } subregions={ this.props.subregions } modal={ this.props.modal } />
+          <FrmR2 onAction={ this.props.onAction } regions={ this.props.regions } subregions={ this.props.subregions } modal={ this.props.modal } />
 
-          <FrmR3 modal={ this.props.modal } />
+          <FrmR3 onAction={ this.props.onAction } modal={ this.props.modal } />
 
-          <FrmR4 modal={ this.props.modal } />
+          <FrmR4 onAction={ this.props.onAction } modal={ this.props.modal } />
 
-          <FrmR5 modal={ this.props.modal } />
+          <FrmR5 onAction={ this.props.onAction } modal={ this.props.modal } />
 
       </BenModal>
 
