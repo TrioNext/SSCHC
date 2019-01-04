f0e07bcf77c39097fdc910391310cb5c4b48a5d0
Hồng Hải <honghai.js6@gmail.com>
2019-01-04 17:56:14 +0700

    Update offModalComp.js

--- src/pages/Company/Office/offModalComp.js
+++ src/pages/Company/Office/offModalComp.js
@@ -17,20 +17,19 @@ function FrmR1(props){
   const form = modal.form ;
   const data = modal.data ;
 
-  const defaultValue = props.onAction === 'post' ? '' : form.code;
 
   return (
     <Row form>
       <Col md={4}>
         <FormGroup>
           <Label id="la-code"> Mã văn phòng <span className="text-danger">*</span></Label>
-          <Input type="text" id="code" onChange={ (e)=>{ modal.onChange('code', e);  } } defaultValue={ defaultValue }  placeholder="Tạo mã" />
+          <Input type="text" id="code" onChange={ (e)=>{ modal.onChange('code', e);  } } defaultValue={ data.code }  placeholder="Tạo mã" />
         </FormGroup>
       </Col>
       <Col md={8}>
         <FormGroup>
           <Label id="la-name"> Tên văn phòng <span className="text-danger">*</span></Label>
-          <Input type="text" id="name" onChange={ (e)=>{ modal.onChange('name', e);  } } defaultValue={ form.name }  placeholder="Nhập tên" />
+          <Input type="text" id="name" onChange={ (e)=>{ modal.onChange('name', e);  } } defaultValue={ data.name }  placeholder="Nhập tên" />
         </FormGroup>
       </Col>
     </Row>
@@ -51,20 +50,20 @@ function FrmR2(props){
       <Col md={4}>
         <FormGroup>
           <Label id="la-phone"> Số ĐT <span className="text-danger">*</span></Label>
-          <Input type="text" id="phone" onChange={ (e)=>{ modal.onChange('phone', e);  } } defaultValue={ form.phone }  placeholder="nhập số ĐT" />
+          <Input type="text" id="phone" onChange={ (e)=>{ modal.onChange('phone', e);  } } defaultValue={ data.phone }  placeholder="nhập số ĐT" />
         </FormGroup>
       </Col>
       <Col md={4}>
         <FormGroup>
           <Label> Tỉnh / Thành </Label>
-            <SelectCity modal={ modal } regions={ props.regions} selected={ form.region_code} />
+            <SelectCity modal={ modal } regions={ props.regions} selected={ data.region_code} />
         </FormGroup>
       </Col>
       <Col md={4}>
         <FormGroup>
           <Label> Quận/Huyện </Label>
 
-            <SelectDist modal={ modal } subregions={ props.subregions }  selected={ form.subregion_code } />
+            <SelectDist modal={ modal } subregions={ props.subregions }  selected={ data.subregion_code } />
         </FormGroup>
       </Col>
     </Row>
@@ -81,12 +80,12 @@ function FrmR3(props){
     <div>
       <FormGroup>
         <Label id="la-address">Địa chỉ <span className="text-danger">*</span></Label>
-          <Input type="text" id="address" onChange={ (e)=>{ modal.onChange('address', e);  } } defaultValue = { form.address } placeholder="Nhập địa chỉ"/>
+          <Input type="text" id="address" onChange={ (e)=>{ modal.onChange('address', e);  } } defaultValue = { data.address } placeholder="Nhập địa chỉ"/>
       </FormGroup>
 
       <FormGroup>
         <Label>IP được chấm công</Label>
-          <Input type="text" onChange={ (e)=>{ modal.onChange('ip_chamcong', e);  } } defaultValue = { form.ip_chamcong }  placeholder="Nhập địa chỉ IP"/>
+          <Input type="text" onChange={ (e)=>{ modal.onChange('ip_chamcong', e);  } } defaultValue = { data.ip_chamcong }  placeholder="Nhập địa chỉ IP"/>
       </FormGroup>
     </div>
   )
@@ -100,7 +99,7 @@ function FrmR4(props){
   const form = modal.form ;
 
 
-  const begin = moment('2018-11-20 '+form.working_begin).format('HH:mm').split(':');
+  const begin = moment('2018-11-20 '+data.working_begin).format('HH:mm').split(':');
 
 
 
@@ -129,7 +128,7 @@ function FrmR5(props){
   const data = modal.data ;
   const form = modal.form ;
 
-  const end = moment('2018-11-20 '+form.working_end).format('HH:mm').split(':');
+  const end = moment('2018-11-20 '+data.working_end).format('HH:mm').split(':');
 
 
   return (
@@ -159,10 +158,6 @@ class OffModalComp extends Component{
   constructor(props){
     super(props);
 
-    this.state = {
-      onAction:''
-    }
-
 
   }
 
@@ -173,15 +168,15 @@ class OffModalComp extends Component{
 
       <BenModal name={this.props.name} onAction={ this.props.onAction } modal={ this.props.modal }  >
 
-          <FrmR1 onAction={ this.props.onAction } modal={ this.props.modal } />
+          <FrmR1  modal={ this.props.modal } />
 
-          <FrmR2 onAction={ this.props.onAction } regions={ this.props.regions } subregions={ this.props.subregions } modal={ this.props.modal } />
+          <FrmR2  regions={ this.props.regions } subregions={ this.props.subregions } modal={ this.props.modal } />
 
-          <FrmR3 onAction={ this.props.onAction } modal={ this.props.modal } />
+          <FrmR3  modal={ this.props.modal } />
 
-          <FrmR4 onAction={ this.props.onAction } modal={ this.props.modal } />
+          <FrmR4  modal={ this.props.modal } />
 
-          <FrmR5 onAction={ this.props.onAction } modal={ this.props.modal } />
+          <FrmR5  modal={ this.props.modal } />
 
       </BenModal>
 
