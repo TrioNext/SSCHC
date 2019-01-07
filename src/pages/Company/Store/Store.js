2a4842e27fc0b7fd5beb6534a5e52ba8949e52ca
Hồng Hải <honghai.js6@gmail.com>
2019-01-07 18:06:03 +0700

    Update Store.js

--- src/pages/Company/Store/Store.js
+++ src/pages/Company/Store/Store.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react';
 import {  Row, Col } from 'reactstrap';
 
 
-
+import store from '../../../redux/store';
 import Model from '../../../model/model';
 
 import storeModalCtrl from './storeModalCtrl';
@@ -14,6 +14,15 @@ import StoreModalComp from './StoreModalComp';
 import moment from 'moment';
 import 'moment/locale/vi';
 
+import { STORES_NAME } from '../../../model/model-name';
+
+import {STORES, REGIONS, SUBREGIONS} from '../../../model/model-mode' ;
+import { POST } from '../../../model/action-mode';
+
+
+const REGION_CODE = '79'; // HCM
+const SUBREGION_CODE = '760'; // quan 1
+
 
 class Store extends Component{
 
@@ -21,17 +30,10 @@ class Store extends Component{
       super(props);
 
 
-      this.name = 'Cửa hàng';
-      this.base = 'stores';
-
-      this.data = {
-        id:0,
-        name:'store',
-        list:[]
-      }
-
       this.state = {
-        name:'store',
+
+        tab: STORES.substring(0, STORES.length - 1) ,
+
         onAction:'',
         status:'',
         onTab:props.onTab,
@@ -39,56 +41,128 @@ class Store extends Component{
         isIniData:false
       }
 
-      this.setup();
+      this.data = {
+        stores:[],
+        regions:[],
+        subregions:[]
+      }
+
+      /* initial WHO */
+      this._doSetup();
     }
 
-    setup(){
-      this.model = new Model(this.base);
-
-      this.model.set('paginate',{
+    _doSetup(){
+      this.Model = new Model(STORES);
+      this.Model.set('paginate',{
         offset:0,
         p:0,
         max:'all',
         is_deleted:0
       })
 
+      /******/
+      this.Regions = new Model(REGIONS);
+      this.Regions.set('paginate',{
+        offset:0,
+        p:0,
+        max:'all',
+        sort_by:'name',
+        sort_type:'asc'
+      });
 
-      this.modal = new storeModalCtrl(this);
+      this.SubRegions = new Model(SUBREGIONS);
+
+      /******************/
+      this.Modal = new storeModalCtrl(this);
+
+      this.listenStore();
     }
 
-    onStateChange(newState){
 
-      /* KEEP PRIVATE DATA*/
-      this.data.list = this.model.getData(this.base) || [] ;
-      this.setState(Object.assign(this.state,newState));
+    /* START WHEN*/
+    /*componentDidMount(){}*/
 
+
+    /* NHẬN lệnh : từ NEW PROPS TỪ BODY OBJECT*/
+    componentWillReceiveProps(newProps){
+
+      if(newProps.onTab===this.state.tab){
+
+        this.initData();
+
+        if(newProps.onAction===POST){
+            this.openModalPost();
+        }
+      }
 
     }
+    /* TRIGGER AFFTER SOMETHING*/
+    componentDidUpdate(prevProps, prevState){}
+    /* DESTROY - REMOVE SOMETHING*/
+    componentWillUnmount(){
+      alert('componentWillUnmount happen: store');
+    }
 
-    onDataChange(list){
+    listenStore(){
+      /* AUTO CONNECT REDUX STORE -> COMPONENT DATA -> REFESH THEM  */
+      store.subscribe(()=>{
 
-      /* TRẢ GIÁ TRỊ VỀ CHO PARENT COMPONENT SỬ DỤNG*/
+        this.data.offices = store.getState().office.list || []  ;
+        this.data.regions = store.getState().region.list || []  ;
+        this.data.subregions = store.getState().subregion.list || []  ;
 
-      this.data.list = list ;
-      //this.props.onDataChange(this.data);
-
-
-      /* RE RENDER : ON DATA CHANGE THÀNH CÔNG */
-      this.onStateChange({
-        status:'done'
+        this.whereStateChange({
+          onAction:'listenStore',
+          status:'realtime'
+        })
       })
     }
+    /* END WHEN*/
 
-    /* COMPONENT NÀY DÙNG MODAL : INTERAC INSIDE  */
-    getInfo(id){
+    /*******HOW********/
+    initData(){
 
-      return this.data.list.find(item=> item.id === id);
+      this.Model.load();
+      this.Regions.load();
+
+      this.whereStateChange({
+        isIniData:true
+      })
 
     }
+
+    openModalPost(){
+
+      this.loadSubRegion(REGION_CODE,(res)=>{
+        this.Modal.open('post');
+        this.whereStateChange({
+          form:this.Model.form,
+          onAction:'post',
+          status:'open_modal'
+        })
+      });
+    }
+    openModalUpdate(data){
+      //alert('sss');
+      //this.data.currentRegionCode = data.region_code;
+
+      this.loadSubRegion(data.region_code,(res)=>{
+        this.Modal.open('put',data);
+        this.whereStateChange({
+          form:data,
+          onAction:'put',
+          status:'open_modal'
+        })
+      });
+
+    }
+
+    /*****END HOW***********/
+
+    /* start WHERE */
 
     BlockItem(props){
       const data = props.data;
-
       const date = moment(data.date_created).format('YYYY-MM-DD HH:mm:ss');
 
 
@@ -100,7 +174,7 @@ class Store extends Component{
                     <div>
                        <span><i className="fa fa-map-pin mr-5"></i> {data.name}</span>
                        <span className="pull-right">
-                         <a className='pointer' onClick={ ()=>{ this.modal.open('put',data) } }> <i className="fa fa-gear"></i> </a>
+                         <a className='pointer' onClick={ ()=>{ this.Modal.open('put',data) } }> <i className="fa fa-gear"></i> </a>
                        </span>
                     </div>
                     <i className="fa fa-phone mr-5"></i> { data.phone === null ? 'n/a' : data.phone } <br/>
@@ -117,99 +191,25 @@ class Store extends Component{
       )
     }
 
-
-
-
-
-    loadStore(){
-      const _this = this ;
-
-      this.model.get((res)=>{
-
-        if(typeof res.count !== 'undefined'){
-            _this.onStateChange({status:'success'})
-        }
-
-      });
-
+    whereStateChange(newState){
+      /* KEEP PRIVATE DATA*/
+      this.setState(Object.assign(this.state,newState));
     }
-
-    componentDidMount(){
-
-
-      //this.loadStore();
-      //this.modal.loadCityList();
-      //this.modal.loadDistrictList(this.modal.form.region_code,()=>{}); // code : tp ho chi minh
-
-    }
-
-    initData(){
-      this.loadStore();
-      this.modal.loadCityList();
-      this.modal.loadDistrictList(this.modal.form.region_code,()=>{}); // code : tp ho chi minh
-
-      this.state.isIniData = true ;
-    }
-
-
-    /* NHẬN lệnh : từ NEW PROPS TỪ BODY OBJECT*/
-    componentWillReceiveProps(newProps){
-
-
-      if(newProps.onTab===this.data.name){
-
-        Object.assign(this.state,newProps);
-
-        if(!this.state.isIniData){
-          this.initData()
-        }
-
-        if(newProps.onAction==='post'){
-            this.modal.open('post');
-        }
-      }
-
-
-
-    }
-
-    /* TRIGGER AFFTER SOMETHING*/
-    componentDidUpdate(prevProps, prevState){
-
-      //alert(JSON.stringifyr);
-      //alert('componentDidUpdate: '+ JSON.stringify(prevProps));
-      //alert('componentDidUpdate');
-      /*alert('ok man : componentDidUpdate');
-      alert(JSON.stringify(prevProps));
-      alert(JSON.stringify(prevState));*/
-
-    }
-
-    /* DESTROY - REMOVE SOMETHING*/
-    componentWillUnmount(){
-      alert('componentWillUnmount happen: store');
-    }
-
-
-
     render(){
 
-        const list = this.data.list ;
-        const modalTitle = this.props.onAction ==='post' ? 'Tạo '+this.name : 'Cập nhật '+this.name;
 
-
-
+        const modalTitle = this.props.onAction ==='post' ? 'Tạo '+ STORES_NAME : 'Cập nhật '+ STORES_NAME;
 
         return(
-            <div hidden={  this.props.onTab === this.data.name ? false : true } >
+            <div hidden={  this.props.onTab === this.state.tab ? false : true } >
 
 
-                 <StoreModalComp name={ modalTitle} onAction={ this.props.onAction} modal={ this.modal } />
+                 <StoreModalComp name={ modalTitle} regions={ this.data.regions } subregions={ this.data.subregions } onAction={ this.props.onAction} modal={ this.Modal } />
 
                  <Row>
 
                     {
-                      list.map((item)=>{
+                      this.data.stores.map((item)=>{
 
 
                         return this.BlockItem({key:item.id, data:item});
