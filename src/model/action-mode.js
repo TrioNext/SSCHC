2403cfdfe88257911d2d54eddf796c581e9e275e
Hồng Hải <honghai.js6@gmail.com>
2019-01-07 18:05:51 +0700

    Create action-mode.js

---
+++ src/model/action-mode.js
@@ -0,0 +1,9 @@
+export const POST = 'post';
+export const PUT = 'put';
+export const DELETE = 'delete';
+export const GET = 'get';
+
+
+export const CREATE = 'create';
+export const UPDATE = 'update';
+export const REMOVE = 'remove';
