6418d4beab79b04e3f4038b921d2c74983aacb0f
honghaijs6 <44365604+honghaijs6@users.noreply.github.com>
2019-01-08 23:06:33 +0700

    Update doLoadRegion.js

--- src/hook/ultil/doLoadRegion.js
+++ src/hook/ultil/doLoadRegion.js
@@ -2,21 +2,17 @@
 import Model from '../../model/model';
 import { REGIONS } from '../../model/model-mode';
 
-export default function(region_code,onSuccess){
+export default function(){
 
   const Regions = new Model(REGIONS);
-
   Regions.set('paginate',{
     offset:0,
     p:0,
     max:'all',
     sort_by:'name',
-    sort_type:'asc',
-    parent_code:region_code
+    sort_type:'asc'
   });
 
-  Regions.get((res)=>{
-    onSuccess(res)
-  })
-
+  Regions.load();
+  
 }
