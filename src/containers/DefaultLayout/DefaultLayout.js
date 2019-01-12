878ec60f590d240dafb9732e78ab0e1bb613589e
Hồng Hải <honghai.js6@gmail.com>
2019-01-12 12:28:25 +0700

    Update DefaultLayout.js

--- src/containers/DefaultLayout/DefaultLayout.js
+++ src/containers/DefaultLayout/DefaultLayout.js
@@ -67,7 +67,7 @@ class DefaultLayout extends Component {
                       : (null);
                   },
                 )}
-                <Redirect from="/" to="/company" />
+                <Redirect from="/" to="/dashboard" />
               </Switch>
 
             </Container>
