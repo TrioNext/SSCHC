f7c86156fe78df0c3cda33c19dae430f0e4ee7b0
Hồng Hải <honghai.js6@gmail.com>
2019-01-14 18:09:56 +0700

    Create index.js

---
+++ src/pages/Order/Setting/index.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+
+class OrderSetting extends Component{
+
+  render(){
+    return (
+      <div className="animated fadeIn">
+        <div className="ubuntu-app " style={{border:0, padding:10}}>
+            <main>
+                Thiết lập
+            </main>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default OrderSetting;
