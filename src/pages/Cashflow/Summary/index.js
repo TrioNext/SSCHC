16bc6bdef2165e7aaab0874575f1dbadd9434fa6
Hồng Hải <honghai.js6@gmail.com>
2019-01-14 18:09:16 +0700

    Create index.js

---
+++ src/pages/Cashflow/Summary/index.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+
+class CashflowSummary extends Component{
+
+  render(){
+    return (
+      <div className="animated fadeIn">
+        <div className="ubuntu-app " style={{border:0, padding:10}}>
+            <main>
+                Tổng quan thu chi
+            </main>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CashflowSummary;
