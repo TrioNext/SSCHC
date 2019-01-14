85ef6d2a7e22472b2b5931503561f2127d151cb7
Hồng Hải <honghai.js6@gmail.com>
2019-01-14 18:09:30 +0700

    Create index.js

---
+++ src/pages/Customer/Point/index.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+
+class Point extends Component{
+
+  render(){
+    return (
+      <div className="animated fadeIn">
+        <div className="ubuntu-app " style={{border:0, padding:10}}>
+            <main>
+                Điểm tích luỹ
+            </main>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Point;
