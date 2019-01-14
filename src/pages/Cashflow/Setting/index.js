f6851845c6fe4b444a30dfcddac5621e6f7e0135
Hồng Hải <honghai.js6@gmail.com>
2019-01-14 18:09:13 +0700

    Create index.js

---
+++ src/pages/Cashflow/Setting/index.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+
+class CashflowSetting extends Component{
+
+  render(){
+    return (
+      <div className="animated fadeIn">
+        <div className="ubuntu-app " style={{border:0, padding:10}}>
+            <main>
+                Thiết lập sổ quỹ
+            </main>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CashflowSetting;
