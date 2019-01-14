2a91602dcdb778b3f2a017941c737366a551e7b5
Hồng Hải <honghai.js6@gmail.com>
2019-01-14 18:09:18 +0700

    Create index.js

---
+++ src/pages/Cashflow/View/index.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+
+class CashflowView extends Component{
+
+  render(){
+    return (
+      <div className="animated fadeIn">
+        <div className="ubuntu-app " style={{border:0, padding:10}}>
+            <main>
+                Phiếu thu - Phiếu chi
+            </main>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CashflowView;
