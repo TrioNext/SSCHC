85c7c5867c543d6a81bf82d33e0dc462970854d9
Hồng Hải <honghai.js6@gmail.com>
2019-01-15 18:11:46 +0700

    Update model-mode.js

--- src/model/model-mode.js
+++ src/model/model-mode.js
@@ -5,3 +5,6 @@ export const OFFICES = 'offices';
 export const STORES = 'stores';
 export const REGIONS = 'regions';
 export const SUBREGIONS = 'subregions';
+
+export const INVENTORIES = 'inventories';
+export const INVENTORY_TRACKS = 'inventorie_tracks';
