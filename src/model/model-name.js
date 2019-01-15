1ffd6b3deceafee164730bda66c0af6b49480c90
Hồng Hải <honghai.js6@gmail.com>
2019-01-15 18:11:47 +0700

    Update model-name.js

--- src/model/model-name.js
+++ src/model/model-name.js
@@ -2,3 +2,6 @@ export const USERS_NAME = 'Thành viên';
 export const DEPARTMENTS_NAME = 'Bộ phận';
 export const STORES_NAME = 'Cửa hàng';
 export const OFFICES_NAME = 'Văn phòng';
+
+export const INVENTORIES_NAME = 'Nhà kho';
+export const INVENTORY_TRACK_NAME = 'Xuất - Nhập Kho';
