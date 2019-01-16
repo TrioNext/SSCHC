1c8942fe2275a833fe0562d38e2d6a16d63a8811
Hồng Hải <honghai.js6@gmail.com>
2019-01-16 18:38:50 +0700

    Update index.js

--- src/pages/Inventory/Setting/index.js
+++ src/pages/Inventory/Setting/index.js
@@ -1,5 +1,7 @@
 
 import React, { Component } from 'react';
+import { Row, Col } from 'reactstrap';
+import { AppSwitch } from '@coreui/react'
 
 class Dashboard extends Component{
 
@@ -7,8 +9,42 @@ class Dashboard extends Component{
     return (
       <div className="animated fadeIn">
         <div className="ubuntu-app " style={{border:0, padding:10}}>
-            <main>
-                Thiết lập
+            <main className="form-general" >
+                <Row>
+                  <Col md="4">
+                    <h5> Đặt hàng </h5>
+                    Thiết lâp liên quan đến đặt hàng
+                  </Col>
+                  <Col md="4" style={{
+                      verticalAlign:'middle'
+                    }}>
+                    <p>Tự động tạo hóa đơn XUẤT sản phẩm</p><br/>
+                    <p> Cấm hủy PHIẾU đã hoàn thành </p>
+                  </Col>
+                  <Col md="4"
+                    style={{
+                        verticalAlign:'middle'
+                      }}
+                    >
+                    <p> <AppSwitch className={'mx-1'} variant={'pill'} color={'primary'}  checked={false} /> </p>
+                    <p> <AppSwitch className={'mx-1'} variant={'pill'} color={'primary'}  checked={false} /> </p>
+                  </Col>
+
+                </Row>
+
+                <Row>
+                  <Col md="4" className="align-middle">
+                    <h5> Phương pháp tồn kho </h5>
+                    Thiết lâp liên quan đến Phương pháp tồn kho
+                  </Col>
+                  <Col md="4" className="align-middle">
+                    <p> Cho phép tồn kho theo Serial Number / IMEI </p>
+                  </Col>
+                  <Col md="4" className="align-middle">
+                    <AppSwitch className={'mx-1'} variant={'pill'} color={'primary'} checked={false} />
+                  </Col>
+
+                </Row>
             </main>
         </div>
       </div>
